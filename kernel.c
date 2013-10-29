d043414fcf9e833b306217742457fb7cc92970a5
Gustavo Castillo <gcastillo@smu.edu>
2013-10-29 02:24:36 -0500

    Added Made the Shell Script and succesfully compiled first test of C

--- kernel.c
+++ kernel.c
@@ -1,19 +1,15 @@
-/// Making the file for the Kernel
-//Gustavo Castillo
-
 
 void printToScreen();
 
 
-int main(int argc, char **argv) {
+int main() {
 	printToScreen();
 	
-	while(true){
+	while(1){
 	};
 }
 
 void printToScreen(){
 	putInMemory(0xB000, 0x8140, 'A');
 	putInMemory(0xB000, 0x8141, 0x7);
-
 }
