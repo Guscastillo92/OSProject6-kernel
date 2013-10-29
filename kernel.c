4924cf32f1efef24db93050bfb3fdfe9da30083e
Gustavo Castillo <gcastillo@smu.edu>
2013-10-29 02:48:43 -0500

    After getting the terminal to print a white C, I finished the project by making it print Hello World. Project 6 Final Commit

--- kernel.c
+++ kernel.c
@@ -1,15 +1,27 @@
 
-void printToScreen();
+void printToScreen(char letter,int location);
 
 
 int main() {
-	printToScreen();
+	printToScreen('H', 0x8140);
+	printToScreen('E', 0x8142);
+	printToScreen('L', 0x8144);
+	printToScreen('L', 0x8146);
+	printToScreen('O', 0x8148);
+	printToScreen(' ', 0x8150);
+	printToScreen('W', 0x8152);
+	printToScreen('O', 0x8154);
+	printToScreen('R', 0x8156);
+	printToScreen('L', 0x8158);
+	printToScreen('D', 0x815A);
+
+
 	
 	while(1){
 	};
 }
 
-void printToScreen(){
-	putInMemory(0xB000, 0x8140, 'A');
-	putInMemory(0xB000, 0x8141, 0x7);
+void printToScreen(char letter, int location){
+	putInMemory(0xB000, location, letter);
+	putInMemory(0xB000, location+1, 0x7);
 }
